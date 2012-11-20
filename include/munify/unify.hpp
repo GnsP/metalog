7fa82047e4980a2af7a49ddb11166d757b3d2104
Bruno Dutra <brunocodutra@gmail.com>
2012-11-20 00:47:12 -0200

    the unification algorithm is mostly done. the next step is to implement type modifiers (*, &, const, volatile) forwarding, so to be possible to unify, for example, X* with a* using the substitution X = a.

--- include/munify/unify.hpp
+++ include/munify/unify.hpp
@@ -7,6 +7,7 @@
 #ifndef _MUNIFY_UNIFY_HPP_
 #define _MUNIFY_UNIFY_HPP_
 
+#include "atom.hpp"
 #include "substitute.hpp"
 
 #include <boost/mpl/bool.hpp>
@@ -14,27 +15,102 @@
 #include <boost/mpl/placeholders.hpp>
 #include <boost/mpl/pair.hpp>
 #include <boost/mpl/map.hpp>
-#include <boost/mpl/insert.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/mpl/joint_view.hpp>
+#include <boost/type_traits.hpp>
 
 namespace munify
 {
+    //base: unification of constants
+    template<typename lExpr, typename rExpr, typename u = boost::mpl::map<> >
+    struct unify : boost::mpl::bool_<boost::is_same<lExpr, rExpr>::value>
+    {
+            typedef boost::mpl::map<> unifiers;
+    };
 
-    template<typename lExpr, typename rExpr, typename unifiers = boost::mpl::map<> >
-    struct unify : unify<typename substitute<unifiers, lExpr>::type, typename substitute<unifiers, rExpr>::type>
+    //{lRel(lExpr_1, lExpr_2, ..., lExpr_n-1, lExpr_n) = rRel(rExpr_1, rExpr_2, ..., rExpr_n-1, rExpr_n)} ==
+    //    {lRel(lExpr_1, _(lExpr_2, _(..., _(lExpr_n-1, lExpr_n)))) = rRel(rExpr_1, _(rExpr_2, _(..., _(rExpr_n-1, rExpr_n))))}
+    template<template<typename...> class lRel, typename lH, typename... lT, template<typename...> class rRel, typename rH, typename... rT, typename u>
+    struct unify<lRel<lH, lT...>, rRel<rH, rT...>, u> : unify<lRel<lH, boost::tuples::tuple<lT...> >, rRel<rH, boost::tuples::tuple<rT...> >, u>
     {};
 
-    template<typename lExpr, typename rExpr>
-    struct unify<lExpr, rExpr, boost::mpl::map<> > : boost::mpl::false_
+    //type modifiers forwarding
+//    template<typename lExpr, typename rExpr>
+//    struct unify<lExpr*, rExpr*, boost::mpl::map<> > : unify<lExpr, rExpr>
+//    {
+//            typedef boost::mpl::map<> unifiers;
+//    };
+
+    //main recursion
+    template<template<typename...> class rel, typename lH, typename lT, typename rH, typename rT, typename u>
+    class unify<rel<lH, lT>, rel<rH, rT>, u> :
+            public boost::mpl::if_
+            <
+                unify<lH, rH, u>,
+                unify<lT, rT, typename unify<lH, rH, u>::unifiers>,
+                boost::mpl::false_
+            >::type
     {
-            typedef boost::mpl::map<> unifiers;
+        private:
+            template<typename, typename>
+            struct lazy;
+
+            template<typename unification>
+            struct lazy<boost::mpl::true_, unification>
+            {
+                    typedef typename unification::unifiers type;
+            };
+
+            template<typename _>
+            struct lazy<boost::mpl::false_, _>
+            {
+                    typedef boost::mpl::map<> type;
+            };
+
+        public:
+            typedef typename boost::mpl::joint_view
+            <
+                typename unify::template lazy<typename unify::type, unify<lH, rH, u> >::type,
+                typename unify::template lazy<typename unify::type, unify<lT, rT, typename unify<lH, rH, u>::unifiers > >::type
+            > unifiers;
     };
 
-    template<typename atom>
-    struct unify<atom, atom, boost::mpl::map<> > : boost::mpl::true_
+    //main recursion shortcuts
+    template<template<typename...> class rel, typename h, typename lT, typename rT, typename u>
+    struct unify<rel<h, lT>, rel<h, rT>, u> : unify<lT, rT, u>
+    {};
+
+    template<template<typename...> class rel, typename lH, typename rH, typename t, typename u>
+    struct unify<rel<lH, t>, rel<rH, t>, u> : unify<lH, rH, u>
+    {};
+
+    template<template<typename...> class rel, typename h, typename t, typename u>
+    struct unify<rel<h, t>, rel<h, t>, u> : boost::mpl::true_
     {
-            typedef boost::mpl::map<> unifiers;
+            typedef u unifiers;
     };
 
+    //unification of atoms
+    template<typename h, typename t, typename u>
+    struct unify<atom<h, t>, atom<h, t>, u> : boost::mpl::true_
+    {
+            typedef u unifiers;
+    };
+
+    template<typename constant, typename u>
+    struct unify<atom<constant>, constant, u> : boost::mpl::true_
+    {
+            typedef u unifiers;
+    };
+
+    template<typename constant, typename u>
+    struct unify<constant, atom<constant>, u> : boost::mpl::true_
+    {
+            typedef u unifiers;
+    };
+
+    //variable binding
     template<int n, typename expr>
     struct unify<boost::mpl::arg<n>, expr, boost::mpl::map<> > : boost::mpl::true_
     {
@@ -42,8 +118,23 @@ namespace munify
             typedef boost::mpl::map<boost::mpl::pair<boost::mpl::arg<n>, expr> > unifiers;
     };
 
-    template<int n, typename expr>
-    struct unify<expr, boost::mpl::arg<n>, boost::mpl::map<> > : unify<boost::mpl::arg<n>, expr>
+    template<int n, typename expr, typename u>
+    struct unify<boost::mpl::arg<n>, expr, u> : unify<typename substitute<u, boost::mpl::arg<n> >::type, typename substitute<u, expr>::type >
+    {
+            typedef boost::mpl::if_
+            <
+                typename unify::type,
+                typename boost::mpl::joint_view
+                <
+                    u,
+                    typename unify<typename substitute<u, boost::mpl::arg<n> >::type, typename substitute<u, expr>::type>::unifiers
+                >,
+                boost::mpl::map<>
+            > unifiers;
+    };
+
+    template<int n, typename expr, typename u>
+    struct unify<expr, boost::mpl::arg<n>, u> : unify<boost::mpl::arg<n>, expr, u>
     {};
 
     template<int m, int n>
@@ -52,25 +143,34 @@ namespace munify
             typedef boost::mpl::map<boost::mpl::pair<boost::mpl::arg<m>, boost::mpl::arg<n> > > unifiers;
     };
 
+    template<int m, int n, typename u>
+    struct unify<boost::mpl::arg<m>, boost::mpl::arg<n>, u> : unify<typename substitute<u, boost::mpl::arg<m> >::type, typename substitute<u, boost::mpl::arg<n> >::type>
+    {
+            typedef boost::mpl::if_
+            <
+                typename unify::type,
+                typename boost::mpl::joint_view
+                <
+                    u,
+                    typename unify<typename substitute<u, boost::mpl::arg<m> >::type, typename substitute<u, boost::mpl::arg<n> >::type>::unifiers
+                >,
+                boost::mpl::map<>
+            > unifiers;
+    };
+
+    //variable binding shortcuts
+    template<int n, typename u>
+    struct unify<boost::mpl::arg<n>, boost::mpl::arg<n>, u> : boost::mpl::true_
+    {
+            typedef u unifiers;
+    };
+
+    //variable binding ambiguity avoidance
     template<int n>
     struct unify<boost::mpl::arg<n>, boost::mpl::arg<n>, boost::mpl::map<> > : boost::mpl::true_
     {
             typedef boost::mpl::map<> unifiers;
     };
-
-//    template<template<typename...> class lRel, typename... lArgs, template<typename...> class rRel, typename... rArgs>
-//    struct unify<lRel<lArgs...>, rRel<rArgs...>, boost::mpl::map<> >
-//    {
-
-
-//    };
-
-    template<template<typename...> class rel, typename... args>
-    struct unify<rel<args...>, rel<args...>, boost::mpl::map<> > : boost::mpl::true_
-    {
-            typedef boost::mpl::map<> unifiers;
-    };
-
 }
 
 #endif
--- test/unify.cpp
+++ test/unify.cpp
@@ -14,16 +14,27 @@ struct rel2;
 template<typename, typename, typename>
 struct rel3;
 
+
 int main()
 {
     std::cout << std::endl;
+    std::cout << "not unifiable:" << std::endl;
     std::cout << "munify::unify<int, void>::value = " << munify::unify<int, void>::value << std::endl;
+    std::cout << "munify::unify<rel2<int, int>, rel2<int, int*> >::value = " << munify::unify<rel2<int, int>, rel2<int, int*> >::value << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "unifiable:" << std::endl;
     std::cout << "munify::unify<void, void>::value = " << munify::unify<void, void>::value << std::endl;
+    std::cout << "munify::unify<munify::atom<void, int, double>, munify::atom<void, int, double> >::value = " << munify::unify<munify::atom<void, int, double>, munify::atom<void, int, double> >::value << std::endl;
     std::cout << "munify::unify<boost::mpl::_1, void>::value = " << munify::unify<boost::mpl::_1, void>::value << std::endl;
+    std::cout << "munify::unify<boost::mpl::_1*, void*>::value = " << munify::unify<boost::mpl::_1*, void*>::value << std::endl;
     std::cout << "munify::unify<boost::mpl::_1, boost::mpl::_1>::value = " << munify::unify<boost::mpl::_1, boost::mpl::_1>::value << std::endl;
     std::cout << "munify::unify<boost::mpl::_1, boost::mpl::_2>::value = " << munify::unify<boost::mpl::_1, boost::mpl::_2>::value << std::endl;
     std::cout << "munify::unify<boost::mpl::_1, rel2<int, int*> >::value = " << munify::unify<boost::mpl::_1, rel2<int, int*> >::value << std::endl;
     std::cout << "munify::unify<rel2<int, int*>, rel2<int, int*> >::value = " << munify::unify<rel2<int, int*>, rel2<int, int*> >::value << std::endl;
-
+    std::cout << "munify::unify<rel2<boost::mpl::_1, boost::mpl::_2>, rel2<int, int*> >::value = " << munify::unify<rel2<boost::mpl::_1, boost::mpl::_2>, rel2<int, int*> >::value << std::endl;
+    std::cout << "munify::unify<rel2<int, boost::mpl::_1>, rel2<int, int*> >::value = " << munify::unify<rel2<int, boost::mpl::_1>, rel2<int, int*> >::value << std::endl;
+    std::cout << "munify::unify<rel2<boost::mpl::_1, int*>, rel2<int, int*> >::value = " << munify::unify<rel2<boost::mpl::_1, int*>, rel2<int, int*> >::value << std::endl;
+    std::cout << "munify::unify<rel2<boost::mpl::_1, boost::mpl::_1*>, rel2<int, int*> >::value = " << munify::unify<rel2<boost::mpl::_1, boost::mpl::_1*>, rel2<int, int*> >::value << std::endl;
     return 0;
 }
