181c54d96cef0f08ac4445c5f7cb04a242c052bc
Bruno Dutra <brunocodutra@gmail.com>
2014-07-12 00:24:40 -0300

    Reducing code verbosity

--- include/metalog/unify/terms.hpp
+++ include/metalog/unify/terms.hpp
@@ -19,11 +19,6 @@
 
 namespace metalog
 {
-    template<template<METALOG_VARIADIC_PARAMS(1, _)> class term, typename lExpr, typename rExpr, typename u>
-    struct unify<term<lExpr>, term<rExpr>, u> :
-            unify<lExpr, rExpr, u>
-    {};
-
     template<typename lExpr, typename rExpr, typename u>
     struct unify
             <
@@ -32,17 +27,6 @@ namespace metalog
                 u
             > :
             unify<lExpr, rExpr, u>
-    {};
-
-    template<template<METALOG_VARIADIC_PARAMS(2, _)> class term,
-             typename lHExpr, typename lTExpr, typename rHExpr, typename rTExpr, typename u>
-    struct unify<term<lHExpr, lTExpr>, term<rHExpr, rTExpr>, u> :
-            boost::mpl::if_
-            <
-                unify<lHExpr, rHExpr, u>,
-                unify<lTExpr, rTExpr, typename unify<lHExpr, rHExpr, u>::unifiers>,
-                unify<lHExpr, rHExpr, u>
-            >::type
     {};
 
     template<typename lHExpr, typename lTExpr, typename rHExpr, typename rTExpr, typename u>
@@ -61,27 +45,32 @@ namespace metalog
             >::type
     {};
 
+    template<template<METALOG_VARIADIC_PARAMS(1, _)> class term, typename lExpr, typename rExpr, typename u>
+    struct unify<term<lExpr>, term<rExpr>, u> :
+            unify<lExpr, rExpr, u>
+    {};
+
 //{term(lExpr_1, lExpr_2, ..., lExpr_n-1, lExpr_n) = term(rExpr_1, rExpr_2, ..., rExpr_n-1, rExpr_n)} ==
 //    {_(lExpr_1, _(lExpr_2, _(..., _(lExpr_n-1, lExpr_n)))) = _(rExpr_1, _(rExpr_2, _(..., _(rExpr_n-1, rExpr_n))))}
 #define METALOG_DEFINE_EQUIVALENCE_TRANSFORM(N) \
-    BOOST_PP_ASSERT(BOOST_PP_LESS_EQUAL(3, N)) \
+    BOOST_PP_ASSERT(BOOST_PP_LESS_EQUAL(2, N)) \
     template \
     < \
             template<METALOG_VARIADIC_PARAMS(N, _)> class term, \
-            typename lH1Expr, typename lH2Expr, typename lH3Expr METALOG_TRAILING_VARIADIC_PARAMS(BOOST_PP_SUB(N, 3), lTExpr), \
-            typename rH1Expr, typename rH2Expr, typename rH3Expr METALOG_TRAILING_VARIADIC_PARAMS(BOOST_PP_SUB(N, 3), rTExpr), \
+            typename lH1Expr, typename lH2Expr METALOG_TRAILING_VARIADIC_PARAMS(BOOST_PP_SUB(N, 2), lTExpr), \
+            typename rH1Expr, typename rH2Expr METALOG_TRAILING_VARIADIC_PARAMS(BOOST_PP_SUB(N, 2), rTExpr), \
             typename u \
     > \
     struct unify \
     < \
-        term<lH1Expr, lH2Expr, lH3Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 3), lTExpr)>, \
-        term<rH1Expr, rH2Expr, rH3Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 3), rTExpr)>, \
+        term<lH1Expr, lH2Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 2), lTExpr)>, \
+        term<rH1Expr, rH2Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 2), rTExpr)>, \
         u \
     > : \
             unify \
             < \
-                metalog::term<lH1Expr, metalog::term<lH2Expr, lH3Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 3), lTExpr)> >, \
-                metalog::term<rH1Expr, metalog::term<rH2Expr, rH3Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 3), rTExpr)> >, \
+                metalog::term<lH1Expr, metalog::term<lH2Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 2), lTExpr)> >, \
+                metalog::term<rH1Expr, metalog::term<rH2Expr METALOG_TRAILING_VARIADIC_ARGS(BOOST_PP_SUB(N, 2), rTExpr)> >, \
                 u \
             > \
     {};
@@ -91,7 +80,7 @@ namespace metalog
 #define METALOG_FORWARD_DEFINE_EQUIVALENCE_TRANSFORM(Z, N, DATA) \
     METALOG_DEFINE_EQUIVALENCE_TRANSFORM(N)
 
-    BOOST_PP_REPEAT_FROM_TO(3, METALOG_MAX_VARIADIC_ARGS, METALOG_FORWARD_DEFINE_EQUIVALENCE_TRANSFORM, _)
+    BOOST_PP_REPEAT_FROM_TO(2, METALOG_MAX_VARIADIC_ARGS, METALOG_FORWARD_DEFINE_EQUIVALENCE_TRANSFORM, _)
 #endif
 
 METALOG_DEFINE_EQUIVALENCE_TRANSFORM(METALOG_MAX_VARIADIC_ARGS)
