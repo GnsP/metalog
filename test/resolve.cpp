ab9fccda631684b9fcf527dd85e8d7eb14712b54
Bruno Dutra <brunocodutra@gmail.com>
2014-07-13 15:37:06 -0300

    Adding test script for the SLD resolve implementation

---
+++ test/resolve.cpp
@@ -0,0 +1,12 @@
+/*
+ * This file is part of metalog, a free software.
+ * Use, modification and distribution is subject to the BSD 2-clause license.
+ * See accompanying file LICENSE.txt for its full text.
+ */
+
+#include "metalog.hpp"
+
+int main()
+{
+    return 0;
+}
