fc0d628d91b7ba7dd2afea8653c63c1fc8c9fdf8
Bruno Dutra <brunocodutra@gmail.com>
2014-07-13 20:19:16 -0300

    First working example
    issue:
    stack overflows once clauses with premises are added - smells like a bug

--- test/resolve.cpp
+++ test/resolve.cpp
@@ -6,7 +6,74 @@
 
 #include "metalog.hpp"
 
+#include <boost/mpl/vector.hpp>
+
+using namespace metalog;
+
+namespace genealogy
+{
+    template<typename, typename>
+    struct grandparent;
+
+    template<typename, typename>
+    struct parent;
+
+    template<typename, typename>
+    struct father;
+
+    template<typename, typename>
+    struct mother;
+
+    template<typename, typename>
+    struct sibling;
+
+    template<typename, typename>
+    struct spouse;
+
+    template<typename, typename>
+    struct uncle;
+
+    template<typename, typename>
+    struct aunt;
+
+    struct marvin;
+    struct randy;
+    struct sharon;
+    struct stan;
+    struct shelly;
+    struct jimbo;
+    struct flo;
+
+    struct A;
+    struct B;
+    struct C;
+    struct D;
+    struct E;
+    struct F;
+    struct G;
+    struct H;
+    struct I;
+    struct J;
+
+    typedef boost::mpl::vector
+    <
+        clause<father<marvin, randy> >,
+        clause<father<randy, stan> >,
+        clause<father<randy, shelly> >,
+        clause<spouse<randy, sharon> >,
+        clause<sibling<randy, jimbo> >,
+        clause<aunt<flo, sharon> >/*,
+        clause<sibling<var<A>, var<B> >, conjunction<parent<var<C>, var<A> >, parent<var<C>, var<B> > > >,
+        clause<parent<var<D>, var<E> >, conjunction<father<var<D>, var<E> > > >,
+        clause<parent<var<F>, var<G> >, conjunction<mother<var<F>, var<G> > > >,
+        clause<grandparent<var<H>, var<I> >, conjunction<parent<var<J>, var<I> >, parent<var<H>, var<J> > > >*/
+    > clauses;
+
+}
+
+struct X;
+
 int main()
 {
-    return 0;
+    return (resolve<genealogy::father<var<X>, genealogy::stan>, genealogy::clauses>::value ? 0 : 1);
 }
