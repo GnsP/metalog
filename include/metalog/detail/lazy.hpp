19328624b5131417075558915d510ff5302bf0f3
Bruno Dutra <brunocodutra@gmail.com>
2014-07-25 23:01:35 -0300

    Making lazy distinguish metafunctions created through binding

--- include/metalog/detail/lazy.hpp
+++ include/metalog/detail/lazy.hpp
@@ -7,6 +7,11 @@
 #ifndef _METALOG_DETAIL_LAZY_HPP_
 #define _METALOG_DETAIL_LAZY_HPP_
 
+#include "preprocessor.hpp"
+
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+
+#include <boost/mpl/bind.hpp>
 #include <boost/mpl/apply_wrap.hpp>
 
 namespace metalog
@@ -14,9 +19,23 @@ namespace metalog
     namespace detail
     {
         template<typename f>
-        struct lazy :
-                boost::mpl::apply_wrap0<f>
+        struct lazy;
+
+#define METALOG_DEFINE_LAZY_BIND(N) \
+        template<typename f METALOG_TRAILING_VARIADIC_PARAMS(N, arg)> \
+        struct lazy<boost::mpl::bind<f METALOG_TRAILING_VARIADIC_ARGS(N, arg)> > : \
+                boost::mpl::apply_wrap0<boost::mpl::bind<f METALOG_TRAILING_VARIADIC_ARGS(N, arg)> > \
         {};
+
+#ifdef BOOST_NO_CXX11_VARIADIC_TEMPLATES
+
+#define METALOG_FORWARD_DEFINE_LAZY_BIND(Z, N, DATA) \
+    METALOG_DEFINE_LAZY_BIND(N)
+
+        BOOST_PP_REPEAT_FROM_TO(0, BOOST_MPL_LIMIT_METAFUNCTION_ARITY, METALOG_FORWARD_DEFINE_LAZY_BIND, _)
+#endif
+
+        METALOG_DEFINE_LAZY_BIND(BOOST_MPL_LIMIT_METAFUNCTION_ARITY)
     }
 }
 
